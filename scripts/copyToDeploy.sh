54e048d476b870ce17a3caa9952281000ef68c37
Maija Lintunokka <maija.lintunokka@gmail.com>
2024-10-01 00:38:17 +0300

    chore: fix deploy script

--- scripts/copyToDeploy.sh
+++ scripts/copyToDeploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
-cd ../../
+cd ../
+yarn build
+cd ../
 rm -rf tenebrie.github.io/exether-locmines/
 cp -r exether-loc-mines/dist/ tenebrie.github.io/exether-locmines/
